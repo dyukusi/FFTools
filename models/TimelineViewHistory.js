93f158d3aabf25aed0b805cc76c474acaf05b2c5
Dyukusi <dyukusin@gmail.com>
2018-09-17 06:12:12 +0900

    add id and created_at column to timeline_view_history

--- models/TimelineViewHistory.js
+++ models/TimelineViewHistory.js
@@ -2,9 +2,15 @@ var appRoot = require('app-root-path');
 var TimelineViewHistory = require(appRoot + '/models/TimelineViewHistory.js').TimelineViewHistory;
 
 exports.TimelineViewHistory = class TimelineViewHistory {
-  constructor(ip, timelineId) {
+  constructor(id, ip, timelineId, createdAt) {
+    this.id = id;
     this.ip = ip;
     this.timelineId = timelineId;
+    this.createdAt = createdAt;
+  }
+
+  getId() {
+    return this.id;
   }
 
   getIp() {
@@ -15,52 +21,7 @@ exports.TimelineViewHistory = class TimelineViewHistory {
     return this.timelineId;
   }
 
-  // static functions
-  // static existsByIpAndTimelineId(ip, timelineId) {
-  //   var d = Q.defer();
-  //
-  //   var con = db.connect(constants.FFXIV_TM_DB_NAME).con;
-  //   con.query("SELECT timeline_id FROM timeline_view_history WHERE ip = ? AND timelineId = ?",
-  //     [ip, timelineId],
-  //     function (err, rows, fields) {
-  //
-  //     if (rows.length) {
-  //       d.resolve(true);
-  //     } else {
-  //       d.resolve(false);
-  //     }
-  //
-  //     con.end();
-  //   });
-  //
-  //   return d.promise;
-  // }
-  //
-  // static insert(ip, timelineId) {
-  //   var d = Q.defer();
-  //
-  //   var con = db.connect(constants.FFXIV_TM_DB_NAME).con;
-  //
-  //   con.query(
-  //     "INSERT IGNORE INTO timeline_view_history SET ?",
-  //     {
-  //       ip: ip,
-  //       timeline_id: timelineId,
-  //     },
-  //     function (error, results, fields) {
-  //       if (error) {
-  //         console.log(error.stack);
-  //         d.reject("DB problem");
-  //
-  //         return;
-  //       }
-  //
-  //       d.resolve(results.affectedRows);
-  //
-  //       con.end();
-  //     }
-  //   );
-  //
-  //   return d.promise;
-  // }
+  getCreatedAt() {
+    return this.createdAt;
+  }
 }
